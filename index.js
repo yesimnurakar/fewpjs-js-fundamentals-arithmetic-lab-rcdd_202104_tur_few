42978747c674646f2d96431b39f9a2a44a2fd00d
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-13 17:38:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 let multiply , num1=31 , num2=2;
 multiply = num1*num2;
-console.log(multiply);
+
 let random = Math.floor(Math.random() * 10) + 1;
-console.log(random);
+
 let mod , num3 = 29 ,num4 = 5;
 mod = num3 % num4;
-console.log(mod);
+
 
 let max = Math.max(1, 2, 6, 9, 20);
