83d10e8a9bedb9d3bf14fbf3a1c2c4d4516f1b95
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-13 17:38:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 
-// Write your code here
+let multiply , num1=31 , num2=2;
+multiply = num1*num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod , num3 = 29 ,num4 = 5;
+mod = num3 % num4;
+let max = Math.max(1, 2, 6, 9, 20);
